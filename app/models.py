281bd5c0a2cc8dbba7c7fa56cbe1876827bdad12
Johann Schneider <joschi.dev7@gmail.com>
2025-09-29 10:51:05 +0200

    worked with the Post Model and the right creating of the database table

--- app/models.py
+++ app/models.py
@@ -1,10 +1,21 @@
 from .database import engine, SessionDep
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Column
+from pydantic import ConfigDict
+from sqlalchemy import Boolean, text, TIMESTAMP
+
 
 class Post1(SQLModel, table=True):
-    __tablename__ = "posts"
     id: int | None = Field(default=None, primary_key=True, nullable=False)
     title: str = Field(nullable=False)
     content: str = Field(nullable=False)
-    published: bool = Field(default=True)
+    published: bool = Field(
+        sa_column=Column(Boolean, nullable=False, server_default=text("TRUE"))
+    ) #this is special for setting a default value in a postgresql database
+    created_at: str = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+        )
+    )
     #created_at: str | None = None
+
+    #model_config = ConfigDict(table_name="posts")
