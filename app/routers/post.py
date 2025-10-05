8f76d1e3347ca0f5f53cb80d8fe04c008214e79b
Johann Schneider <joschi.dev7@gmail.com>
2025-10-05 15:24:57 +0200

    more restrictions and authentications

--- app/routers/post.py
+++ app/routers/post.py
@@ -13,9 +13,19 @@ router = APIRouter(
 
 @router.get("/", response_model=list[PostRead])
 def get_posts(session: SessionDep, current_user = Depends(tm.get_current_user)):
-    posts = session.exec(select(Post)).all()
+    posts = session.exec(select(Post).filter(Post.owner_id == current_user.id)).all()
 
     return posts
+
+@router.get("/{id}", response_model=PostRead)
+def get_post(id: int, session: SessionDep, current_user= Depends(tm.get_current_user)):
+    post = session.get(Post, id)
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+    return post
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostRead)
 def create_post(post: PostCreate, session: SessionDep, current_user = Depends(tm.get_current_user)):
@@ -30,15 +40,6 @@ def create_post(post: PostCreate, session: SessionDep, current_user = Depends(tm
     return new_post
 
 
-@router.get("/{id}", response_model=PostRead)
-def get_post(id: int, session: SessionDep, current_user= Depends(tm.get_current_user)):
-    post = session.get(Post, id)
-
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-
-    return post
-
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, session: SessionDep, current_user = Depends(tm.get_current_user)):
 
@@ -46,14 +47,14 @@ def delete_post(id: int, session: SessionDep, current_user = Depends(tm.get_curr
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-
-    if current_user.id == post.owner_id:
-        session.delete(post)
-        session.commit()
-
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
-    else:
+    if post.owner_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+    
+    session.delete(post)
+    session.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 @router.put("/{id}", response_model=PostRead, status_code=status.HTTP_200_OK)
 def update_post(id: int, post: PostUpdate, session: SessionDep, current_user = Depends(tm.get_current_user)):
@@ -61,13 +62,13 @@ def update_post(id: int, post: PostUpdate, session: SessionDep, current_user = D
 
     if not db_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    
-    if current_user.id == db_post.owner_id:
-        db_post.sqlmodel_update(post.model_dump(exclude_unset=True))
-        session.add(db_post)
-        session.commit()
-        session.refresh(db_post)
-
-        return db_post
-    else:
+    if post.owner_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
+    
+    db_post.sqlmodel_update(post.model_dump(exclude_unset=True))
+    session.add(db_post)
+    session.commit()
+    session.refresh(db_post)
+
+    return db_post
+    
